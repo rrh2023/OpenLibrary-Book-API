cef7e60689f5d895fd25e4c75b120b1981f91e73
Roberto <rrh2023@bu.edu>
2025-10-24 23:24:57 -0400

    error handling

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,32 +1,66 @@
+const ErrorResponse = require('../utils/errorResponse');
 const Book = require('../models/Book')
 
 // @desc Get all books
 // @route GET /api/v1/book
 // @access Public
-exports.getBooks = (req,res,next) => {
-    res.status(200).json({success: true, msg: 'Show all books', hello: req.hello})
+exports.getBooks = async (req,res,next) => {
+    try {
+        const books = await Book.find()
+
+        res.status(200).json({success: true, count:books.length, data: books})
+    } catch (error) {
+        next(error);
+    }
 }
 
 // @descs Get single book
 // @route GET /api/v1/book/:id
 // @access Public
-exports.getBook = (req, res, next) => {
-    Bootcamp.create(req.body)
-    res.status(200).json({success: true, msg: `Show book ${req.params.id}`})
+exports.getBook = async (req, res, next) => {
+    try {
+        const book = await Book.findById(req.params.id);
+
+        if(!book){
+            return next(new ErrorResponse(`Book not found with id of ${req.params.id}`, 404));
+        }else{
+            res.status(200).json({success: true, data: book})
+        }
+
+        
+    } catch (error) {
+        next(error);
+    }
 }
 
 // @desc Create new book
 // @route POST /api/v1/book
 // @access Private
-exports.createBook = (req, res, next) => {
-    console.log(req.body)
-    res.status(200).json({success: true, msg: 'Create new book'})
+exports.createBook = async (req, res, next) => {
+    try {
+        const book = await Book.create(req.body)
+        res.status(201).json({success: true, data: book})
+    } catch (error) {
+        next(error);
+    }
+    
 }
 
 // @desc  Delete book
 // @route DELETE /api/v1/book/:id
 // @access Private
-exports.deleteBook = (req, res, next) => {
-    res.status(200).json({success:true, msg: `Delete book ${req.params.id}`})
+exports.deleteBook = async (req, res, next) => {
+
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+
+        if(!book){
+            return next(new ErrorResponse(`Book not found with id of ${req.params.id}`, 404));
+        }else{
+            res.status(200).json({success: true, data: book})
+        }
+    } catch (error) {
+        next(error);
+    }
 }
 
