4adb2fda35d42f9bcb3f7180f3edb5f5c642874d
Roberto <rrh2023@bu.edu>
2025-10-26 11:08:19 -0400

    including authors

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -42,7 +42,7 @@ exports.getBooks = asyncHandler(async (req,res,next) => {
 
     // Pagination
     const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 100;
+    const limit = parseInt(req.query.limit, 10) || 25;
     const startIndex = (page - 1) * limit;
     const endIndex = page * limit;
     const total = await Bootcamp.countDocuments()
